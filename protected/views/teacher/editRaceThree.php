8bd1b1f23c0e6065a7490b060f45f2a1dbdee01a
chrisgxf <550273171@qq.com>
2016-12-21 19:32:28 +0800

    fix
    老师端添加上传文本txt

--- protected/views/teacher/editRaceThree.php
+++ protected/views/teacher/editRaceThree.php
@@ -42,12 +42,12 @@
         <h3></h3>
         <form class="form-horizontal" method="post" action="./index.php?r=teacher/editRace&indexID=<?php echo $_GET['indexID']; ?>&step=3" id="myForm" enctype="multipart/form-data"> 
             
-            <div class="control-group">
+<!--            <div class="control-group">
                 <label class="control-label">分数：</label>
                 <div class="controls">
                     <textarea name="score" style="width:50px; height:20px;" id="score" ><?php echo $race['score']; ?></textarea> 分
                 </div>
-            </div>
+            </div>-->
             
             <input type="hidden" name="<?php echo ini_get("session.upload_progress.name"); ?>" value="test" />
             <fieldset>
@@ -73,33 +73,33 @@
                     </div>
 <!--                    (支持mp3及wav格式,最大1G)-->
                 </div>
-                <div class="control-group">
+<!--                <div class="control-group">
                     <label class="control-label" for="input03">听打答案：</label>
                     <div class="controls">               
                         <textarea name="content" style="width:450px; height:200px;" id="input03"><?php echo $race['content']; ?></textarea>
                         <br>字数：<span id="wordCount">0</span> 字
                     </div>
-                </div> 
+                </div> -->
             </fieldset>
             
             <!--第二个音频 -->           
-            <div class="control-group">
+<!--            <div class="control-group">
                 <label class="control-label">分数：</label>
                 <div class="controls">
                     <textarea name="score2" style="width:50px; height:20px;" id="score2" ><?php echo $race2['score']; ?></textarea> 分
                 </div>
-            </div>
+            </div>-->
             
             <input type="hidden" name="<?php echo ini_get("session.upload_progress.name"); ?>" value="test" />
             <fieldset>
                 <div class="control-group">
                     <label class="control-label" for="input02s">文件：</label>
                     <div class="controls">
-                        <?php if ($race != "") { ?>
+                        <?php if ($race2 != "") { ?>
                         <div class="control-group">
-                                <?php $listenpath = "./resources/race/" . $race2['resourseID']; ?>
-                                <?php if (file_exists($listenpath)) { ?>
-                                    <video  src = "<?php echo $listenpath; ?>" preload = "auto" style="border:1px solid #5C595A" poster="./resources/race/01d32256f4084132f875a944080917.gif" controls></video>
+                                <?php $listenpath2 = "./resources/race/" . $race2['resourseID']; ?>
+                                <?php if (file_exists($listenpath2)) { ?>
+                                    <video  src = "<?php echo $listenpath2; ?>" preload = "auto" style="border:1px solid #5C595A" poster="./resources/race/01d32256f4084132f875a944080917.gif" controls></video>
                                 <?php } else { ?>
                                     <p style="color: red">原音频文件丢失或损坏！</p>
                                 <?php } ?>
@@ -115,9 +115,15 @@
 <!--                    (支持mp3及wav格式,最大1G)-->
                 </div>
                 <div class="control-group">
+                <label class="control-label" for="input04">上传答案</label>
+                <div class="controls">
+                    <input type="file" name="myfile" id="myfile" >
+                </div>
+            </div>
+                <div class="control-group">
                     <label class="control-label" for="input03s">听打答案：</label>
                     <div class="controls">               
-                        <textarea name="content2" style="width:450px; height:200px;" id="input03s"><?php echo $race2['content']; ?></textarea>
+                        <textarea name="content2" style="width:450px; height:200px;" id="content2"><?php echo $race['content']; ?></textarea>
                         <br>字数：<span id="wordCount">0</span> 字
                     </div>
                 </div> 
@@ -130,23 +136,43 @@
 </div>
 
 <script>
+    <?php 
+    $tag = "0";
+    $tag2 = "0";
+    if($race == NULL){
+       $tag = "1"; 
+    }
+    if($race2 == NULL){
+        $tag2 = "1";
+    }
+    ?>
+    var tag = <?php echo $tag; ?>;
+    var tag2 = <?php echo $tag2; ?>;
     $("#myForm").submit(function () {
         var uploadFile = $("#input02")[0].value;
-        var time = document.querySelector("#name").value;
-        var score = document.querySelector("#score").value;
-        var reg = new RegExp("^[0-9]*$");
-        if (!(reg.test(time) && reg.test(score))) {
-            event.preventDefault();
-            window.wxc.xcConfirm('请输入数字', window.wxc.xcConfirm.typeEnum.info);
-        }
-        if (uploadFile === "")
+        var uploadFile2 = $("#input02s")[0].value;
+//        var time = document.querySelector("#name").value;
+//        var score = document.querySelector("#score").value;
+//        var reg = new RegExp("^[0-9]*$");
+//        if (!(reg.test(time) && reg.test(score))) {
+//            event.preventDefault();
+//            window.wxc.xcConfirm('请输入数字', window.wxc.xcConfirm.typeEnum.info);
+//        }
+        
+        if (uploadFile === "" && tag=="1" )
         {
             window.wxc.xcConfirm('上传文件不能为空', window.wxc.xcConfirm.typeEnum.warning);
             return false;
         }
-
-        var A = $("#input03")[0].value;
-        if (A === "") {
+        
+        if (uploadFile2 === "" && tag2=="1")
+        {
+            window.wxc.xcConfirm('上传文件不能为空', window.wxc.xcConfirm.typeEnum.warning);
+            return false;
+        }
+        var files =  document.getElementById("myfile").value;
+        var content = document.getElementById("content2").value;
+        if (files === "" && content ==="") {
             window.wxc.xcConfirm('内容不能为空', window.wxc.xcConfirm.typeEnum.warning);
             return false;
         }
@@ -168,6 +194,10 @@
         $("#wordCount").text(v);
         $("#upload").hide();
         var result = <?php echo "'$result'"; ?>;
+        var result2 = <?php echo "'$result2'"; ?>;
+        var result3 = <?php echo "'$result3'"; ?>;
+        var result4 = <?php echo "'$result4'"; ?>;
+        
         if (result === '1')
             window.wxc.xcConfirm("操作成功！", window.wxc.xcConfirm.typeEnum.success, {
                 onOk: function () {
@@ -180,8 +210,14 @@
                     window.location.href = "./index.php?r=teacher/RaceLst";
                 }
             });
-        }else if(result !=""){
-            window.wxc.xcConfirm(result, window.wxc.xcConfirm.typeEnum.error);
+        }else if(result4 !=""){
+            window.wxc.xcConfirm(result4, window.wxc.xcConfirm.typeEnum.error);
+        }
+        else if(result2 !=""){
+            window.wxc.xcConfirm(result2, window.wxc.xcConfirm.typeEnum.error);
+        }
+        else if(result3 !=""){
+            window.wxc.xcConfirm(result3, window.wxc.xcConfirm.typeEnum.error);
         }
     });
 
