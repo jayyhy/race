568fb9048ec7b3c296081a96b567d33a3505aae6
SSS2009 <1197321479@qq.com>
2016-12-22 09:20:14 +0800

    fix
    管理员新UI

--- protected/views/layouts/adminBar.php
+++ protected/views/layouts/adminBar.php
@@ -8,6 +8,7 @@ if (isset(Yii::app()->session['userid_now']) && Yii::app()->session['role_now']=
         <head>
             <meta charset="utf-8">
             <title>亚伟速录</title>
+<!--            <link rel='icon' href='pic.ico ' type=‘image/x-ico’ /> -->
             <link href="<?php echo CSS_URL; ?>bootstrap.min.css" rel="stylesheet">
             <link href="<?php echo CSS_URL; ?>site.css" rel="stylesheet">
             <script src="<?php echo JS_URL; ?>jquery.min.js" ></script>
@@ -16,24 +17,14 @@ if (isset(Yii::app()->session['userid_now']) && Yii::app()->session['role_now']=
 <!--            改变alter样式-- extensions/xcConfirm 工具包下-- --> 
                 <link rel="stylesheet" type="text/css" href="<?php echo XC_Confirm; ?>css/xcConfirm.css"/>
 		<script src="<?php echo XC_Confirm; ?>js/xcConfirm.js"  type="text/javascript" charset="utf-8"></script>
-<!--            -->
-             <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
         </head>
         <body>
-            <div  class="container">
-                <div class="navbar">
-                    <div class="navbar-inner">
-                        <div class="container">
-                            <a class="brand" href="./index.php?r=admin/index"></a>
-                            <div class="nav-collapse">
-                                <ul class="nav">
-<!--                                     <li><a id="course_manager"  href="./index.php?r=admin/courseLst">考场调度</a></li>
-                                     <li><a id="topic_manager"  href="./index.php?r=admin/raceLst">试卷调配</a></li>
-                                   <li><a id="notice_manager" href="./index.php?r=admin/noticeLst">公告管理</a></li>-->
-                                      <li><a id="notice_manager" style="width: 344px">&nbsp;</a></li>
-                                      <li><a id="blank_admin"></a></li>
-                                    <li >
-                                        <div class="userUI">
+            <div class="ywnav">
+               
+                    <a class="ywlogo" href="./index.php?r=admin/index"> <div class="ywlogo"> </div></a>
+               
+                <font class="ysgs">亚伟国赛管理系统</font>
+                <div class="userUI">
                                             <a href="" id="userUI" data-toggle="dropdown" title="<?php echo Yii::app()->session['userName']; ?>">
                                                 
                                                 <?php $name=Yii::app()->session['userName'];
@@ -51,24 +42,13 @@ if (isset(Yii::app()->session['userid_now']) && Yii::app()->session['role_now']=
                                                     <a href="./index.php?r=user/login&exit=1">退出</a>
                                                 </li>
                                             </ul>   
-                                        </div>
-                                    </li>
-                                </ul>
-
-                            </div>
-                        </div>
-                    </div>
-                  
-                </div>
-                <div class="row" style="min-height: 700px">
-                    <?php echo $content; ?>
+                                        </div></div>
+                <?php echo $content; ?>
                     
-                </div>
-                  <div style="float: bottom"  class="copyright">
+<!--                  <div style="float: bottom"  class="copyright">
                       2015 &copy;南京兜秘网络科技有限公司.&nbsp;&nbsp;&nbsp;<a href="#"  class="copyright">法律声明</a><a href="#"  class="copyright">联系我们</a><a href="#"  class="copyright">获得帮助</a>
-	</div>
-            </div>
-           
+	</div>-->
+                
         </body>
         
     </html>
